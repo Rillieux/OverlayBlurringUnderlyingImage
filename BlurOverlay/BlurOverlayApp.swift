f9b67983da5533b2321f13a36125f04b981449a4
Rillieux <dave.kondris@gmail.com>
2021-01-31 10:38:05 +0100

    Initial Commit

---
+++ BlurOverlay/BlurOverlayApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlurOverlayApp.swift
+//  BlurOverlay
+//
+//  Created by Dave Kondris on 31/01/21.
+//
+
+import SwiftUI
+
+@main
+struct BlurOverlayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
